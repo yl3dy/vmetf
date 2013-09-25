0a45f574bdfe543efa3edf0b50f0344ed7a73832
Alexander Kiselyov <yl3gdy@archlinux.us>
2013-09-25 20:37:20 +0400

    Lax-Wedroff seeminlgy ok

--- task1.py
+++ task1.py
@@ -63,26 +63,36 @@ def simple_1st_order():
 
 def lax_wendroff():
     f_linear, f_linear_old = empty([GRID_X_SIZE]), empty([GRID_X_SIZE])
-    f_linear[0] = F2
-    f_linear_old = exact_solution(0.)
-    f_plus_lin, f_minus_lin = 0, F2
+    f_nonlin, f_nonlin_old = empty([GRID_X_SIZE]), empty([GRID_X_SIZE])
+    f_linear[0], f_nonlin[0] = F2, F2
+    f_linear_old, f_nonlin_old = exact_solution(0.), exact_solution(0.)
     for n in range(GRID_T_SIZE):
         f_exact = exact_solution(T_VALUES[n])
         if n == 0:
             plot_result(n, f_exact, f_exact, f_exact)
             continue
+        f_plus_lin, f_minus_lin = F2, F2
+        f_plus_nonlin, f_minus_nonlin = F2, F2
         for i in range(1, GRID_X_SIZE-1):
             f_plus_lin = 0.5 * (f_linear_old[i] + f_linear_old[i+1]) - \
                          (A*TAU)/(2*H) * (f_linear_old[i+1] -
                                           f_linear_old[i])
             f_linear[i] = f_linear_old[i] - (A*TAU/H) * (f_plus_lin -
                                                          f_minus_lin)
+            f_plus_nonlin = 0.5 * (f_nonlin_old[i] + f_nonlin_old[i+1]) - \
+                            (TAU)/(2*H) * (f_nonlin_old[i+1]**2 -
+                                           f_nonlin_old[i]**2)
+            f_nonlin[i] = f_nonlin_old[i] - (TAU/H) * (f_plus_nonlin**2 -
+                                                              f_minus_nonlin**2)
+            f_plus_lin, f_minus_lin = f_minus_lin, f_plus_lin
+            f_plus_nonlin, f_minus_nonlin = f_minus_nonlin, f_plus_nonlin
         f_linear[-1] = f_linear[-2]
         f_linear, f_linear_old = f_linear_old, f_linear
-        f_plus_lin, f_minus_lin = f_minus_lin, f_plus_lin
+        f_nonlin[-1] = f_nonlin[-2]
+        f_nonlin, f_nonlin_old = f_nonlin_old, f_nonlin
         # do not plot too often
         if n % int(floor(GRID_T_SIZE / PLOT_NUM)) == 0:
-            plot_result(n, f_exact, f_linear, f_exact)
+            plot_result(n, f_exact, f_linear, f_nonlin)
 
 def clean_output():
     system('rm task1-out/*')
