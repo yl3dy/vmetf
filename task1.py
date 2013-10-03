70b2384cdf919f40d190078d2246362700ec2485
Alexander Kiselyov <yl3gdy@archlinux.us>
2013-10-03 19:29:59 +0400

    Added matplotlib animation

--- task1.py
+++ task1.py
@@ -4,10 +4,11 @@ import sys
 from numpy import empty, linspace, float64
 from math import floor
 import matplotlib.pyplot as plt
+from matplotlib.animation import ArtistAnimation
 from os import system
 
 # Constants:
-A = 1
+A = 0.5
 X0 = 0.1
 F1 = 0
 F2 = 1
@@ -16,9 +17,9 @@ L = 1
 H = 0.01
 # time
 T = 1
-TAU = 0.001
+TAU = 0.01
 
-PLOT_NUM = 20    # number of iters to plot
+PLOT_NUM = 100    # number of iters to plot
 
 GRID_X_SIZE = floor(L / H) + 1
 GRID_T_SIZE = floor(T / TAU) + 1
@@ -32,6 +33,20 @@ def plot_result(t, data1, data2, data3):
     plt.savefig('task1-out/{}.png'.format(t))
     plt.clf()
 
+class AnimatedPlot:
+    sequence = []
+    fig = plt.figure()
+    def __init__(self):
+        plt.axis([0., L, -0.1, F2*1.1])
+    def add_frame(self, t, data1, data2, data3):
+        self.sequence.append(plt.plot(X_VALUES, data1, 'r',
+                                      X_VALUES, data2, 'b',
+                                      X_VALUES, data3, 'g'))
+    def finalize(self):
+        animated_seq = ArtistAnimation(self.fig, self.sequence,
+                                       interval=200, blit=True)
+        plt.show()
+
 def exact_solution(t):
     """Analytical view at some t."""
     f_grid = empty([GRID_X_SIZE])
@@ -41,6 +56,7 @@ def exact_solution(t):
 
 def simple_1st_order():
     """Simple 1st order method."""
+    anim = AnimatedPlot()
     f_linear, f_linear_old = empty([GRID_X_SIZE]), empty([GRID_X_SIZE])
     f_nonlin, f_nonlin_old = empty([GRID_X_SIZE]), empty([GRID_X_SIZE])
     f_linear[0] = f_nonlin[0] = F2
@@ -49,7 +65,7 @@ def simple_1st_order():
     for n in range(GRID_T_SIZE):
         f_exact = exact_solution(T_VALUES[n])
         if n == 0:
-            plot_result(n, f_exact, f_exact, f_exact)
+            anim.add_frame(n, f_exact, f_exact, f_exact)
             continue
         for i in range(1, GRID_X_SIZE):
             f_linear[i] = f_linear_old[i] - (A*TAU / H) * (f_linear_old[i] -
@@ -59,9 +75,11 @@ def simple_1st_order():
         f_nonlin, f_nonlin_old = f_nonlin_old, f_nonlin
         # do not plot too often
         if n % int(floor(GRID_T_SIZE / PLOT_NUM)) == 0:
-            plot_result(n, f_exact, f_linear, f_nonlin)
+            anim.add_frame(n, f_exact, f_linear, f_nonlin)
+    anim.finalize()
 
 def lax_wendroff():
+    anim = AnimatedPlot()
     f_linear, f_linear_old = empty([GRID_X_SIZE]), empty([GRID_X_SIZE])
     f_nonlin, f_nonlin_old = empty([GRID_X_SIZE]), empty([GRID_X_SIZE])
     f_linear[0], f_nonlin[0] = F2, F2
@@ -69,7 +87,7 @@ def lax_wendroff():
     for n in range(GRID_T_SIZE):
         f_exact = exact_solution(T_VALUES[n])
         if n == 0:
-            plot_result(n, f_exact, f_exact, f_exact)
+            anim.add_frame(n, f_exact, f_exact, f_exact)
             continue
         f_plus_lin, f_minus_lin = F2, F2
         f_plus_nonlin, f_minus_nonlin = F2, F2
@@ -92,7 +110,8 @@ def lax_wendroff():
         f_nonlin, f_nonlin_old = f_nonlin_old, f_nonlin
         # do not plot too often
         if n % int(floor(GRID_T_SIZE / PLOT_NUM)) == 0:
-            plot_result(n, f_exact, f_linear, f_nonlin)
+            anim.add_frame(n, f_exact, f_linear, f_nonlin)
+    anim.finalize()
 
 def clean_output():
     system('rm task1-out/*')
@@ -120,3 +139,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# vim:set tw=0:
