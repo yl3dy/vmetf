87b4a9fd908c9ceb757672c8a9e4c792aa7b9c75
Alexander Kiselyov <yl3gdy@archlinux.us>
2013-12-05 19:47:13 +0400

    TASK 3: Added 2nd type boundary conditions

--- task3.py
+++ task3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from numpy import empty, linspace, ones
+from numpy import empty, linspace, ones, zeros
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,16 +14,20 @@ T_NORTH = 2.0
 T_SOUTH = 2.0
 T_EAST = 2.0
 T_WEST = 2.0
-# Power of heat sources
-S_C = 5
-S_P = 10
-S = lambda T: S_C + S_P*T
+# Additional boundary conditions
+BOX_SIZE_1 = [0.2, 0.2]     # box size for 1st type boundary
+BOX_COORDS_2 = [[0.3, 0.3], [0.7, 0.7]]
+T_2 = 2
 # Iterative parameters
 EPS = 1e-4
 TAU = 0.001
-
 # Very small number for lambdas
 L_EPS = 1e-20
+
+# Precalculate useful constants
+BOX_1 = [BOX_SIZE_1[0]/L_X * X_DENSITY, BOX_SIZE_1[1]/L_Y * Y_DENSITY]
+BOX_2 = [[BOX_COORDS_2[0][0]/L_X*X_DENSITY, BOX_COORDS_2[0][1]/L_Y*Y_DENSITY],
+         [BOX_COORDS_2[1][0]/L_X*X_DENSITY, BOX_COORDS_2[1][1]/L_Y*Y_DENSITY]]
 
 def TDMA_solve(main_diag, upper_diag, lower_diag, b):
     """Tridiagonal linear system solver."""
@@ -45,20 +49,32 @@ def TDMA_solve(main_diag, upper_diag, lower_diag, b):
 
     return solution
 
-def set_lambdas_raw():
-    """Set thermal conductivity field."""
-    lambdas_raw = ones([X_DENSITY, Y_DENSITY]) * 1.0
-    lambdas_raw[:20, :20] = 1e20
-    return lambdas_raw
-
-def set_initial_conditions():
+def set_initial_conditions(boundary_type):
     """Set initial conditions for solver."""
     x_grid = linspace(0, L_X, X_DENSITY)
     y_grid = linspace(0, L_Y, Y_DENSITY)
     T_field = ones([X_DENSITY, Y_DENSITY])
+    # set boundary conditions
+    T_field[:, -1] = T_NORTH
+    T_field[:, 0] = T_SOUTH
+    T_field[0, :] = T_WEST
+    T_field[-1, :] = T_EAST
 
-    # calculate lambdas
-    lambdas_raw = set_lambdas_raw()
+    # set raw lambdas
+    lambdas_raw = ones([X_DENSITY, Y_DENSITY]) * 1.0
+    if boundary_type == 1:
+        lambdas_raw[:BOX_1[0], :BOX_1[1]] = 1e20
+    elif boundary_type == 2:
+        lambdas_raw[BOX_2[0][0]:BOX_2[1][0], BOX_2[0][1]:BOX_2[1][1]] = 1e15
+
+    # heat sources
+    s_c = zeros([X_DENSITY, Y_DENSITY])
+    s_p = zeros([X_DENSITY, Y_DENSITY])
+    if boundary_type == 2:
+        s_c[BOX_2[0][0]:BOX_2[1][0], BOX_2[0][1]:BOX_2[1][1]] = 1e20 * T_2
+        s_p[BOX_2[0][0]:BOX_2[1][0], BOX_2[0][1]:BOX_2[1][1]] = -1e20
+
+    # calculate lambdas +/-
     lambdas_x = empty([X_DENSITY - 1, Y_DENSITY - 2])
     lambdas_y = empty([X_DENSITY - 2, Y_DENSITY - 1])
     lambdas_x = ((2 * lambdas_raw[1:, 1:-1] * lambdas_raw[:-1, 1:-1] + L_EPS) /
@@ -66,13 +82,7 @@ def set_initial_conditions():
     lambdas_y = ((2 * lambdas_raw[1:-1, 1:] * lambdas_raw[1:-1, :-1] + L_EPS) /
                  (lambdas_raw[1:-1, 1:] + lambdas_raw[1:-1, :-1] + L_EPS))
 
-    # set boundary conditions
-    T_field[:, -1] = T_NORTH
-    T_field[:, 0] = T_SOUTH
-    T_field[0, :] = T_WEST
-    T_field[-1, :] = T_EAST
-
-    return x_grid, y_grid, lambdas_x, lambdas_y, T_field
+    return x_grid, y_grid, lambdas_x, lambdas_y, T_field, s_c, s_p
 
 def accuracy_acquired(T_next, T_prev):
     """Estimate difference between last 2 iterations."""
@@ -83,9 +93,9 @@ def pretty_plot(T_field):
     plt.imshow(T_field.T, origin='lower', interpolation='none')
     plt.show()
 
-def solver():
+def solver(boundary_type):
     """Entry point for solving."""
-    x_grid, y_grid, lambdas_x, lambdas_y, T_field_prev = set_initial_conditions()
+    x_grid, y_grid, lambdas_x, lambdas_y, T_field_prev, s_c, s_p = set_initial_conditions(boundary_type)
     T_field_next = T_field_prev.copy()          # for n+1 step
     T_field_halfnext = T_field_prev.copy()      # for n+1/2 step
     b_x, b_y = empty(X_DENSITY), empty(Y_DENSITY)    # free coefs for TDMA
@@ -95,16 +105,16 @@ def solver():
     main_diag_y = main_diag_x.copy()
     upper_diag_y, lower_diag_y = upper_diag_x.copy(), lower_diag_x.copy()
 
-    for iteration in range(100):
+    for iteration in range(10):
         # step in X direction
         for k in range(1, Y_DENSITY-1):
             #b_x[1:-1] = T_field_prev[1:-1, k] * 2/TAU + \
                         #(lambdas_y[:, k]/(y_grid[k] - y_grid[k+1])**2 * (T_field_prev[2:, k+1] - T_field_prev[1:-1, k])) - \
                         #(lambdas_y[:, k-1]/(y_grid[k-1] - y_grid[k])**2 * (T_field_prev[1:-1, k] - T_field_prev[:-2, k-1]))
-            b_x[1:-1] = T_field_prev[1:-1, k] * 2/TAU
+            b_x[1:-1] = T_field_prev[1:-1, k] * 2/TAU - s_c[1:-1, k]
             b_x[0], b_x[-1] = T_field_prev[0, k], T_field_prev[-1, k]
             main_diag_x[0], main_diag_x[-1] = 1, 1
-            main_diag_x[1:-1] = 2 / TAU + \
+            main_diag_x[1:-1] = 2 / TAU + s_p[1:-1, k] + \
                                 lambdas_x[:-1, k-1]/(x_grid[1:-1] - x_grid[:-2])**2 + \
                                 lambdas_x[1:, k-1]/(x_grid[2:] - x_grid[1:-1])**2
             upper_diag_x[0] = 0
@@ -118,10 +128,10 @@ def solver():
             #b_y[1:-1] = T_field_halfnext[i, 1:-1] * 2/TAU + \
                         #(lambdas_x[i, :]/(x_grid[i] - x_grid[i+1])**2 * (T_field_halfnext[i+1, 2:] - T_field_halfnext[i, 1:-1])) - \
                         #(lambdas_x[i-1, :]/(x_grid[i-1] - x_grid[i])**2 * (T_field_halfnext[i, 1:-1] - T_field_halfnext[i-1, :-2]))
-            b_y[1:-1] = T_field_halfnext[i, 1:-1] * 2/TAU
+            b_y[1:-1] = T_field_halfnext[i, 1:-1] * 2/TAU - s_c[i, 1:-1]
             b_y[0], b_y[-1] = T_field_halfnext[i, 0], T_field_halfnext[i, -1]
             main_diag_y[0], main_diag_y[-1] = 1, 1
-            main_diag_y[1:-1] = 2 / TAU + \
+            main_diag_y[1:-1] = 2 / TAU + s_p[i, 1:-1] + \
                                 lambdas_y[i-1, :-1]/(y_grid[1:-1] - y_grid[:-2])**2 + \
                                 lambdas_y[i-1, 1:]/(y_grid[2:] - y_grid[1:-1])**2
             upper_diag_y[0] = 0
@@ -133,9 +143,12 @@ def solver():
         T_field_prev = T_field_next.copy()
 
     print(T_field_next)
-    print('Min value: {}, max value: {}'.format(np.min(T_field_next.ravel()), np.max(T_field_next.ravel())))
+    print('Min/max temperature: {}, {}'.format(np.min(T_field_next.ravel()),
+                                               np.max(T_field_next.ravel())))
     pretty_plot(T_field_next)
 
 
 if __name__ == '__main__':
-    solver()
+    solver(2)
+
+# vim: set tw=0:
